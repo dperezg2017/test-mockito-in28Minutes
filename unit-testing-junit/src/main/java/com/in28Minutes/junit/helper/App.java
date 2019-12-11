95f2ebcff1765d3ee49c8885e8a35a2666c9786b
dperezg2017 <dperezg2017@gmail.com>
2019-12-11 00:06:55 -0500

    clases

---
+++ unit-testing-junit/src/main/java/com/in28Minutes/junit/helper/App.java
@@ -0,0 +1,13 @@
+package com.in28Minutes.junit.helper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
