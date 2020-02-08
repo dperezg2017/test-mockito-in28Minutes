1758c502fd323cd8cb97cbd48a4ccccc93ced75e
dperezg2017 <dperezg2017@gmail.com>
2020-02-08 02:35:50 -0500

    d

---
+++ mockito-course/src/main/java/com/in28minutes/mockito/App.java
@@ -0,0 +1,13 @@
+package com.in28minutes.mockito;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
